b5bc28dbd78be31f71ba5bda0cbd591e2b90f548
prasannakumar494 <nprasannakumar.n@gmail.com>
2024-01-24 00:13:29 -0600

    First Commit
    First initial Spring boot commit

---
+++ src/main/java/com/employee/timesheets/TimesheetsApplication.java
@@ -0,0 +1,13 @@
+package com.employee.timesheets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimesheetsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimesheetsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/employee/timesheets/TimesheetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employee.timesheets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimesheetsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
